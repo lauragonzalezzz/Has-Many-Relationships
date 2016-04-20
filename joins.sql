78b23ce087efa9a5cb352a2a229ec1ddf8ba83c3
Laura <lg3295@gmail.com>
2016-04-19 16:39:17 -1000

    completes 3

--- joins.sql
+++ joins.sql
@@ -3,3 +3,10 @@ SELECT * FROM users;
 
 --2
 SELECT * FROM posts WHERE user_id = 100;
+
+--3
+SELECT posts.*, users.username, users.last_name
+FROM posts
+INNER JOIN users
+ON users.id = posts.user_id
+WHERE user_id = 200;
