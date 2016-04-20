b10c5378bef532b141344ed04e3ec57df890eb5d
Laura <lg3295@gmail.com>
2016-04-19 19:19:23 -1000

    completes first additional challenge

--- joins.sql
+++ joins.sql
@@ -76,3 +76,22 @@ ON users.id = posts.user_id
 INNER JOIN comments
 ON posts.id = comments.posts_id
 WHERE comments.body LIKE '%SSL%' AND posts.content LIKE '%dolorum%';
+
+--13
+SELECT users.first_name as post_author_first_name, users.last_name as post_author_last_name, posts.title as post_title, users.username as comment_author_username, comments.body as comment_body
+FROM users
+INNER JOIN posts
+ON users.id = posts.user_id
+INNER JOIN comments
+ON posts.id = comments.posts_id
+WHERE (comments.body LIKE '%firewall%' OR comments.body LIKE '%SSL%')
+AND (posts.content LIKE '%nemo%');
+
+--1
+SELECT posts.id, posts.title, users.id
+FROM users
+INNER JOIN posts
+ON users.id = posts.user_id
+INNER JOIN comments
+ON posts.id = comments.posts_id
+WHERE comments.user_id = posts.user_id;
